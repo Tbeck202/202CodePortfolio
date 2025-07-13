1e401adb0477c022848e4c6de9537ff0a22461d7
Tyler Beck <tbeck202@gmail.com>
2025-07-13 15:29:28 -0600

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+$('.ui.dropdown')
+  .dropdown({
+      on: 'hover'
+  });
+
+$('#avatar')
+  .transition('horizontal flip in', '2000ms')
+;
+
+// $('.tiny')
+//   .transition({
+//     animation : 'zoom in',
+//     interval  : 200
+//   })
+// ;
+
+$('.tiny')
+  .transition('horizontal flip in', '2000ms')
+;
+
+
+
+
+
+
+
+
